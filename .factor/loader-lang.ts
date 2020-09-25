47edd1192812e9924dfc0eb8548fc5b1a9ea9700
robertluffy <71714931+robertluffy@users.noreply.github.com>
2020-09-25 13:55:10 -0500

    Add files via upload

---
+++ .factor/loader-lang.ts
@@ -0,0 +1,8 @@
+import { default as blog_en } from "@factor/plugin-blog/factor-lang-en" // 100
+import { default as blog_es_1 } from "@factor/plugin-blog/factor-lang-es" // 100
+
+
+export default {
+  en: [blog_en],
+  es: [blog_es_1]
+}
---
+++ .factor/loader-settings.ts
@@ -0,0 +1,11 @@
+import { default as dashboard } from "@factor/dashboard/factor-settings" // 100
+import { default as templates } from "@factor/templates/factor-settings" // 100
+import { default as ui } from "@factor/ui/factor-settings" // 100
+import { default as contactForm } from "@factor/plugin-contact-form/factor-settings" // 100
+import { default as blog } from "@factor/plugin-blog/factor-settings" // 100
+import { default as tailwind } from "@factor/plugin-tailwind/factor-settings" // 100
+import { default as zeno } from "@factor/theme-zeno/src/factor-settings" // 150
+import { default as cwd } from "../src/factor-settings" // 1000
+
+
+export default [ dashboard, templates, ui, contactForm, blog, tailwind, zeno, cwd ]
